9dbb636a1a42678e13e846ed116735f0943d5500
Peter Smythe <peter@afrigis.co.za>
2024-03-05 21:04:26 +0200

    Fix 2 sec timeout problem in production

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ async function storeKV(key, value) {
 
 // This function runs before a transaction.
   const beforeTransaction = async (authorization) => {
+    // transactionStart = new Date().getTime(); 
     // Immediately authorise any purchases under the limit
     if (authorization.centsAmount <= process.env.transactionLimitCents) {
         return true;
@@ -31,19 +32,21 @@ async function storeKV(key, value) {
 
     var prev_value = await getKV(key);
     if (prev_value.status == 404) {
-        await storeKV(key, value);
-        return false // Decline the first transaction
+        /*await*/ storeKV(key, value);  // Do not wait before declining, because storing takes too long ( > 2 seconds)
+		//transactionEnd = new Date().getTime(); 
+        //console.log('Transaction duration: ' + (transactionEnd - transactionStart));
+        return false; // Decline the first transaction
     }
 
     const firstTrans = await prev_value.json();
     // console.log(firstTrans);
     if ((now - firstTrans.now)/1000 > process.env.transactionWindowSeconds) {
         // console.log('Too long ago: ' + (now - firstTrans.now)/1000);
-        await storeKV(key, value);
-        return false // Decline, and save as the first transaction
+        /*await*/ storeKV(key, value);
+        return false; // Decline, and save as the first transaction
     }
 
-    await storeKV(key, value); // Store as most recent, in case this transaction is now declined due to differentCard logic
+    /*await*/ storeKV(key, value); // Store as most recent, in case this transaction is now declined due to differentCard logic
     if (process.env.differentCard == "true"){   // Grrr, workaround env.json forcing everything to a string
         // console.log(cardId !== firstTrans.cardId)
         return (cardId !== firstTrans.cardId);
